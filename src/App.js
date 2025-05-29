8c3c7048dbaac44c20fdfd55c04ce2daf61f6ae2
Mahipal Kachhad <mahipalkac@gmail.com>
2025-05-29 19:08:08 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const addData = () => {};
+
   return (
     <div className="App">
-
+      <p>
+        name :{" "}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </p>
+      <input type="button" value="Add Data" onClick={addData} />
     </div>
   );
 }
